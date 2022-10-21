eec7dee6220c7765d0871263df4f0fa9d1970bd1
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2022-10-21 11:33:08 +0900

    mysql

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+  return HttpResponse("Hello world.")
+
+def index(request):
+    return render(request, "blog/index.html")
+
+def detail(request):
+    return HttpResponse("detail page")
