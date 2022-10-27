3a53393881c4a5c8fb8f0f4b6ec8457c7333ce7e
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2022-10-27 23:43:06 +0900

    ログイン画面遷移

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
   return HttpResponse("Hello world.")
@@ -9,3 +13,27 @@ def index(request):
 
 def detail(request):
     return HttpResponse("detail page")
+class AccountCreateView(View):
+    def get(self, request):
+        return render(request, "blog/register.html")
+    def post(self, request):
+        # ユーザー情報を保存する
+        User.objects.create_user(
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
+        return render(request, "blog/register_success.html")
+class AccountLoginView(LoginView):
+    """ログインページのテンプレート"""
+    template_name = 'blog/login.html'
+    def get_default_redirect_url(self):
+        """ログインに成功した時に飛ばされるURL"""
+        return "/blog"
+class MypageView(View):
+    def get(self, request):
+        return render(request, "blog/mypage.html")
+class MypageView(LoginRequiredMixin, View):
+    # ログインしていない場合に飛ばすページの設定
+    login_url = '/blog/login'
+    def get(self, request):
+        return render(request, "blog/mypage.html")
